0e425df9fff3a8cb52df81f3d22c7d26e1d189d3
Run <1440401918@qq.com>
2019-06-18 11:09:15 +0800

    first commit

---
+++ gateway.cpp
@@ -0,0 +1,13 @@
+#include "gateway.h"
+
+gateway::gateway()
+{
+  type ="dashboard";
+  gateway_id = "123761354321";
+  temperature = 0;
+  humidity = 0;
+  fan = 0;
+  beam = 0;
+  light1 = false;
+  light2 = false;
+}
